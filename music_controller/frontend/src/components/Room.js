4cd32241d273f83cd4c9188501315a514c838a45
Aakash Chahal <aakash.chahal62@gmail.com>
2021-03-21 16:55:40 +0530

    calling API endpoints using react

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -29,8 +29,8 @@ export default class Room extends Component {
             <div>
                 <h3>{this.roomCode}</h3>
                 <p>Votes: {this.state.votesToSkip}</p>
-                <p>Guest Can Pause: {this.state.guestCanPause.toSring()}</p>
-                <p>Host: {this.state.isHost.toSring()}</p>
+                <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
+                <p>Host: {this.state.isHost.toString()}</p>
             </div>
         );
     }
