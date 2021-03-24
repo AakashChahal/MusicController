65c3f6aa95e98649eef5d777d85d18b29543dd4d
Aakash Chahal <aakash.chahal62@gmail.com>
2021-03-24 07:57:39 +0530

    pausing and playing functionality

--- music_controller/frontend/src/components/MusicPlayer.js
+++ music_controller/frontend/src/components/MusicPlayer.js
@@ -1,48 +1,79 @@
 import React, { Component } from "react";
 import {
-  Grid,
-  Typography,
-  Card,
-  IconButton,
-  LinearProgress,
+    Grid,
+    Typography,
+    Card,
+    IconButton,
+    LinearProgress,
 } from "@material-ui/core";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import PauseIcon from "@material-ui/icons/Pause";
 import SkipNextIcon from "@material-ui/icons/SkipNext";
 
 export default class MusicPlayer extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    const songProgress = (this.props.time / this.props.duration) * 100;
+    playSong() {
+        const requestOptions = {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+        };
+        fetch("/spotify/play", requestOptions);
+    }
 
-    return (
-      <Card>
-        <Grid container alignItems="center">
-          <Grid item align="center" xs={4}>
-            <img src={this.props.image_url} height="100%" width="100%" />
-          </Grid>
-          <Grid item align="center" xs={8}>
-            <Typography component="h5" variant="h5">
-              {this.props.title}
-            </Typography>
-            <Typography color="textSecondary" variant="subtitle1">
-              {this.props.artist}
-            </Typography>
-            <div>
-              <IconButton>
-                {this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon />}
-              </IconButton>
-              <IconButton>
-                <SkipNextIcon />
-              </IconButton>
-            </div>
-          </Grid>
-        </Grid>
-        <LinearProgress variant="determinate" value={songProgress} />
-      </Card>
-    );
-  }
+    pauseSong() {
+        const requestOptions = {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+        };
+        fetch("/spotify/pause", requestOptions);
+        console.log("song paused");
+    }
+
+    render() {
+        const songProgress = (this.props.time / this.props.duration) * 100;
+
+        return (
+            <Card>
+                <Grid container alignItems="center">
+                    <Grid item align="center" xs={4}>
+                        <img
+                            src={this.props.image_url}
+                            height="100%"
+                            width="100%"
+                        />
+                    </Grid>
+                    <Grid item align="center" xs={8}>
+                        <Typography component="h5" variant="h5">
+                            {this.props.title}
+                        </Typography>
+                        <Typography color="textSecondary" variant="subtitle1">
+                            {this.props.artist}
+                        </Typography>
+                        <div>
+                            <IconButton
+                                onClick={() => {
+                                    this.props.is_playing
+                                        ? this.pauseSong()
+                                        : this.playSong();
+                                }}
+                            >
+                                {this.props.is_playing ? (
+                                    <PauseIcon />
+                                ) : (
+                                    <PlayArrowIcon />
+                                )}
+                            </IconButton>
+                            <IconButton>
+                                <SkipNextIcon />
+                            </IconButton>
+                        </div>
+                    </Grid>
+                </Grid>
+                <LinearProgress variant="determinate" value={songProgress} />
+            </Card>
+        );
+    }
 }
