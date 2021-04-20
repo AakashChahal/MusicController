dbf2eacbee4f9db849701b854e399fd82f9ddb4d
Aakash Chahal <aakash.chahal62@gmail.com>
2021-04-20 12:48:42 +0530

    UPDATE: using user session now

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
-import SignIn from "./SignIn";
 
 export default class App extends Component {
     constructor(props) {
@@ -11,7 +10,7 @@ export default class App extends Component {
     render() {
         return (
             <div className="center">
-                <SignIn />
+                <HomePage />
             </div>
         );
     }
