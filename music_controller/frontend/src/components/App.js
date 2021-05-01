7ffd89cdd4a058cbcaf9b30cb95c2ea2e06f54d6
Aakash Chahal <aakash.chahal62@gmail.com>
2021-05-01 23:41:37 +0530

    App.js changed

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,20 +1,101 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
-import HomePage from "./HomePage";
+import React, { useState, useEffect } from "react";
+import fire from "./fire";
+import Login from "./Login";
+import Hero from "./Hero";
+import "./App.css";
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+function App() {
+    const [user, setUser] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [hasAccount, setHasAccoutn] = useState("false");
 
-    render() {
-        return (
-            <div className="center">
-                <HomePage />
-            </div>
-        );
-    }
+    const clearInputs = () => {
+        setEmail("");
+        setPassword("");
+    };
+
+    const clearErrors = () => {
+        setEmailError("");
+        setPasswordError("");
+    };
+
+    const handleLogin = () => {
+        clearErrors();
+        fire.auth()
+            .signInWithEmailAndPassword(email, password)
+            .catch((err) => {
+                switch (err.code) {
+                    case "auth/invalid-email":
+                    case "auth/user-disabled":
+                    case "auth/user-not-found":
+                        setEmailError(err.message);
+                        break;
+                    case "auth/wrong-password":
+                        setPasswordError(err.message);
+                        break;
+                }
+            });
+    };
+
+    const handleSignup = () => {
+        clearErrors();
+        fire.auth()
+            .createUserWithEmailAndPassword(email, password)
+            .catch((err) => {
+                switch (err.code) {
+                    case "auth/email-already-in-use":
+                    case "auth/invalid-email":
+                        setEmailError(err.message);
+                        break;
+                    case "auth/wrong-password":
+                        setPasswordError(err.message);
+                        break;
+                }
+            });
+    };
+
+    const handleLogOut = () => {
+        fire.auth().signOut();
+    };
+
+    const authListner = () => {
+        fire.auth().onAuthStateChanged((user) => {
+            if (user) {
+                clearInputs();
+                setUser(user);
+            } else {
+                setUser("");
+            }
+        });
+    };
+
+    useEffect(() => {
+        authListner();
+    }, []);
+
+    return (
+        <div className="App">
+            {user ? (
+                <Hero handleLogOut={handleLogOut} />
+            ) : (
+                <Login
+                    email={email}
+                    setEmail={setEmail}
+                    password={password}
+                    setPassword={setPassword}
+                    handleLogin={handleLogin}
+                    handleSignup={handleSignup}
+                    hasAccount={hasAccount}
+                    setHasAccoutn={setHasAccoutn}
+                    emailError={emailError}
+                    passwordError={passwordError}
+                />
+            )}
+        </div>
+    );
 }
 
-const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+export default App;
